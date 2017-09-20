d53d1c99c34dafd12b30e97217b3002262136d05
See Soon Kiat <a0124552@u.nus.edu>
2017-09-20 19:33:00 +0800

    use loads

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
+const _ = require("lodash");
 
 router.post('/create', function(req, res){
 	var source = ['POST /user/create'];
